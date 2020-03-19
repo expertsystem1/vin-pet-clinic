0d48ec3fed5707c6ea22bbb8a66db861fc0e395c
Vincenzo <vincenzolucente@gmail.com>
2020-03-19 11:51:16 +0100

    initial setupe

---
+++ src/main/java/guru/springframework/vinpetclinic/VinPetClinicApplication.java
@@ -0,0 +1,13 @@
+package guru.springframework.vinpetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VinPetClinicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VinPetClinicApplication.class, args);
+	}
+
+}
---
+++ src/test/java/guru/springframework/vinpetclinic/VinPetClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package guru.springframework.vinpetclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VinPetClinicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

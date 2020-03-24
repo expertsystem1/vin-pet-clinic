d5920c33faeeba8852be9a974ecf1ef2da461d0b
Vincenzo <vincenzolucente@gmail.com>
2020-03-24 15:25:36 +0100

    request mapping at class level

--- pet-clinic-web/src/main/java/guru/springframework/vinpetclinic/controllers/PetController.java
+++ pet-clinic-web/src/main/java/guru/springframework/vinpetclinic/controllers/PetController.java
@@ -3,10 +3,11 @@ package guru.springframework.vinpetclinic.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+@RequestMapping("/pets")
 @Controller
 public class PetController {
 	
-	@RequestMapping({"/pets","/pets/index","/pets/index.html"})
+	@RequestMapping({"/","/index","/index.html",""})
 	public String index() {
 		return "pets/index";
 	}

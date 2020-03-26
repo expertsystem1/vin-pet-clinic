91999c2fe40e6ccaa5941f65eabd5d9e7449c071
Vincenzo <vincenzolucente@gmail.com>
2020-03-26 18:00:50 +0100

    implement spring data jpa owner service

---
+++ pet-clinic-data/src/main/java/guru/springframework/vinpetclinic/services/jpa/OwnerSDJPA.java
@@ -0,0 +1,63 @@
+package guru.springframework.vinpetclinic.services.jpa;
+
+import java.util.HashSet;
+import java.util.Optional;
+import java.util.Set;
+
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+import guru.springframework.vinpetclinic.pojo.Owner;
+import guru.springframework.vinpetclinic.repositories.OwnerRepository;
+import guru.springframework.vinpetclinic.services.OwnerService;
+
+@Service
+@Profile("springdatajpa")
+public class OwnerSDJPA implements OwnerService{
+
+	private final OwnerRepository ownerRepository;
+
+	public OwnerSDJPA(OwnerRepository ownerRepository) {
+		this.ownerRepository = ownerRepository;
+	}
+
+	@Override
+	public Set<Owner> findAll() {
+		Set<Owner> owners = new HashSet<Owner>();
+		ownerRepository.findAll().forEach(owners::add);
+		return owners;
+	}
+
+	@Override
+	public Owner findById(Long id) {
+		Optional<Owner> optionalOwner = this.ownerRepository.findById(id); 
+		if(optionalOwner.isPresent()) {
+			return optionalOwner.get();
+		}
+		return null;
+	}
+
+	@Override
+	public Owner save(Owner object) {
+		return ownerRepository.save(object);
+	}
+
+	@Override
+	public void delete(Owner object) {
+		this.ownerRepository.delete(object);
+
+	}
+
+	@Override
+	public void deleteById(Long id) {
+		this.deleteById(id);
+
+	}
+
+	@Override
+	public Owner findByLastName(String lastName) {
+		return this.ownerRepository.findByLastName(lastName);
+	}
+
+
+}

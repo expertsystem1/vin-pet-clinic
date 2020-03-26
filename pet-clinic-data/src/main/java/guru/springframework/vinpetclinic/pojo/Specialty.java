fdeffd9144893a942eee997da4bf5d10b31b61f0
Vincenzo <vincenzolucente@gmail.com>
2020-03-26 17:33:48 +0100

    created vet entities

--- pet-clinic-data/src/main/java/guru/springframework/vinpetclinic/pojo/Specialty.java
+++ pet-clinic-data/src/main/java/guru/springframework/vinpetclinic/pojo/Specialty.java
@@ -1,8 +1,15 @@
 package guru.springframework.vinpetclinic.pojo;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="specialities")
 public class Specialty extends BaseEntity {
 
 	private static final long serialVersionUID = 1L;
+	@Column(name="description")
 	private String description;
 	
 	public String getDescription() {
--- pet-clinic-data/src/main/java/guru/springframework/vinpetclinic/pojo/Vet.java
+++ pet-clinic-data/src/main/java/guru/springframework/vinpetclinic/pojo/Vet.java
@@ -3,9 +3,27 @@ package guru.springframework.vinpetclinic.pojo;
 import java.util.HashSet;
 import java.util.Set;
 
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="vets")
 public class Vet extends Person{
 	
 	private static final long serialVersionUID = 1L;
+	
+	@ManyToMany(fetch = FetchType.EAGER)
+	@JoinTable(
+	   name="vet_specialities",
+	   joinColumns = @JoinColumn(name = "vet_id"),
+	   inverseJoinColumns = @JoinColumn(name = "specialty_id")
+	)
 	private Set<Specialty> specialties = new HashSet<Specialty>();
 	
 	public Set<Specialty> getSpecialties() {

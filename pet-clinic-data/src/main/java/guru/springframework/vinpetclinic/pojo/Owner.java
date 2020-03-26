e7052794fe967482d46f484e862d1a52af347ccb
Vincenzo <vincenzolucente@gmail.com>
2020-03-26 08:38:23 +0100

    added Visit entity

--- pet-clinic-data/src/main/java/guru/springframework/vinpetclinic/pojo/Owner.java
+++ pet-clinic-data/src/main/java/guru/springframework/vinpetclinic/pojo/Owner.java
@@ -1,6 +1,19 @@
 package guru.springframework.vinpetclinic.pojo;
 
-public class Owner extends Person{
-	private static final long serialVersionUID = 1L;
+import java.util.Set;
 
+public class Owner extends Person{
+	
+	private static final long serialVersionUID = 1L;
+	private Set<Pet> pets;
+	
+	public Set<Pet> getPets() {
+		return pets;
+	}
+	public void setPets(Set<Pet> pets) {
+		this.pets = pets;
+	}	
+	
+	
+	
 }

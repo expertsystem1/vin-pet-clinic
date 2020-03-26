ab13fd0576e1f2476ec2234981140fdaa52388ad
Vincenzo <vincenzolucente@gmail.com>
2020-03-26 10:11:26 +0100

    specialities loaded

--- pet-clinic-data/src/main/java/guru/springframework/vinpetclinic/services/map/VetServiceMap.java
+++ pet-clinic-data/src/main/java/guru/springframework/vinpetclinic/services/map/VetServiceMap.java
@@ -4,15 +4,32 @@ import java.util.Set;
 
 import org.springframework.stereotype.Service;
 
+import guru.springframework.vinpetclinic.pojo.Specialty;
 import guru.springframework.vinpetclinic.pojo.Vet;
+import guru.springframework.vinpetclinic.services.SpecialtyService;
 import guru.springframework.vinpetclinic.services.VetService;
 
 @Service
 public class VetServiceMap extends AbstractMapService<Vet, Long> implements VetService{
+	
+	private final SpecialtyService specialtyService;
+	
+	public VetServiceMap(SpecialtyService specialtyService) {
+		super();
+		this.specialtyService = specialtyService;
+	}
 
 	@Override
 	public Vet save(Vet object) {
 		if (object != null) {
+			if(object.getSpecialties() != null && object.getSpecialties().size() > 0) {
+				object.getSpecialties().forEach(specialty -> {
+					if (specialty.getId() == null) {
+						Specialty savedSpeciality = specialtyService.save(specialty);
+						specialty.setId(savedSpeciality.getId());
+					}
+				});
+			}
 			super.save(object);
 		}
 		return object;
--- pet-clinic-web/src/main/java/guru/springframework/vinpetclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/guru/springframework/vinpetclinic/bootstrap/DataLoader.java
@@ -11,10 +11,12 @@ import org.springframework.stereotype.Component;
 import guru.springframework.vinpetclinic.pojo.Owner;
 import guru.springframework.vinpetclinic.pojo.Pet;
 import guru.springframework.vinpetclinic.pojo.PetType;
+import guru.springframework.vinpetclinic.pojo.Specialty;
 import guru.springframework.vinpetclinic.pojo.Vet;
 import guru.springframework.vinpetclinic.services.OwnerService;
 import guru.springframework.vinpetclinic.services.PetService;
 import guru.springframework.vinpetclinic.services.PetTypeService;
+import guru.springframework.vinpetclinic.services.SpecialtyService;
 import guru.springframework.vinpetclinic.services.VetService;
 
 @Component
@@ -24,52 +26,74 @@ public class DataLoader implements CommandLineRunner{
 	private final VetService vetService;
 	private final PetService petService;
 	private final PetTypeService petTypeService;
-		
+	private final SpecialtyService specialtyService;
 
-	public DataLoader(OwnerService ownerService, VetService vetService, PetTypeService petTypeService,PetService petService) {
+
+
+	public DataLoader(OwnerService ownerService, VetService vetService, PetTypeService petTypeService,PetService petService, SpecialtyService specialtyService) {
 		this.ownerService = ownerService;
 		this.vetService = vetService;
 		this.petService = petService;
 		this.petTypeService = petTypeService;
+		this.specialtyService = specialtyService;
 	}
 
 	@Override
 	public void run(String... args) throws Exception {
-		
+		int count = petService.findAll().size();
+		if (count == 0) {
+			loadData();
+		}
+	}
+
+	private void loadData() {
 		PetType dog = new PetType();
 		dog.setName("Dog");
 		petTypeService.save(dog);
-		
+
 		PetType cat = new PetType();
 		cat.setName("Cat");
 		petTypeService.save(cat);
-		
+
 		PetType parrot = new PetType();
 		parrot.setName("Parrot");
 		petTypeService.save(parrot);
-		
+
+		Specialty reptiles = new Specialty();
+		reptiles.setDescription("Reptiles");
+
+		Specialty homePets = new Specialty();
+		homePets.setDescription("homePets");
+
+		Specialty flyiingAnimals = new Specialty();
+		flyiingAnimals.setDescription("flyingAnimals");
+
+		specialtyService.save(flyiingAnimals);
+		specialtyService.save(homePets);
+		specialtyService.save(reptiles);
+
 		Pet fido = new Pet();
 		fido.setName("Fido");
 		fido.setPetType(dog);
 		fido.setBirthDate(LocalDate.of(2014, Month.JANUARY, 1));	
-		
+
 		Pet tom = new Pet();
 		tom.setName("Tom");
 		tom.setPetType(cat);
 		fido.setBirthDate(LocalDate.of(2015, Month.FEBRUARY, 3));	
-		
+
 		Pet cocorito = new Pet();
 		cocorito.setName("Cocorito");
 		cocorito.setPetType(parrot);
 		fido.setBirthDate(LocalDate.of(2016, Month.JUNE, 26));	
-		
+
 		Owner owner1 = new Owner();
 		owner1.setFirstName("Michael");
 		owner1.setLastName("Weston");
 		owner1.setCity("Bari");
 		owner1.setTelephone("+39 080 551 7254");
 		owner1.setAddress("Via Dei Mille n.21 70010");
-		
+
 		Set<Pet> owner1Pets = new HashSet<Pet>();
 		owner1Pets.add(fido);
 		owner1Pets.add(cocorito);
@@ -77,38 +101,42 @@ public class DataLoader implements CommandLineRunner{
 		fido.setOwner(owner1);
 		cocorito.setOwner(owner1);
 		ownerService.save(owner1);
-				
+
 		Owner owner2 = new Owner();
 		owner2.setFirstName("Fiona");
 		owner2.setLastName("Gleannane");
 		owner1.setCity("Zurich");
 		owner1.setTelephone("+41 254 258 11");
 		owner1.setAddress("St. Jackob Strasse n.39 8004");
-			
+
 		Set<Pet> owner2Pets = new HashSet<Pet>();
 		owner2Pets.add(tom);
 		owner1.setPets(owner2Pets);
 		tom.setOwner(owner2);
 		ownerService.save(owner2);
-		
-		/*petService.save(cocorito);
-		petService.save(tom);
-		petService.save(fido);*/
-		
+
 		Vet vet1 = new Vet();
 		vet1.setFirstName("Sam");
 		vet1.setLastName("Axe");
+		Set<Specialty> specialities = new HashSet<Specialty>();
+		specialities.add(homePets);
+		specialities.add(flyiingAnimals);
+		vet1.setSpecialties(specialities);
 		vetService.save(vet1);
-		
+
 		Vet vet2 = new Vet();
 		vet2.setFirstName("Naomi");
 		vet2.setLastName("Campbell");
+		Set<Specialty> specialities2 = new HashSet<Specialty>();
+		specialities2.add(reptiles);
+		specialities2.add(flyiingAnimals);
+		vet1.setSpecialties(specialities2);
 		vetService.save(vet2);
+
 		System.out.println("Loaded Owners....");
+		System.out.println("Loaded Specialties....");
 		System.out.println("Loaded Vets....");
 		System.out.println("Loaded Pet's types....");
 		System.out.println("Loaded Pets....");
-		
 	}
-
 }

d61e0a2208946c24e2008cc1ada87b02978c1c2c
Vincenzo <vincenzolucente@gmail.com>
2020-03-26 08:58:50 +0100

    added PetType Service

---
+++ pet-clinic-data/src/main/java/guru/springframework/vinpetclinic/services/PetTypeService.java
@@ -0,0 +1,7 @@
+package guru.springframework.vinpetclinic.services;
+
+import guru.springframework.vinpetclinic.pojo.PetType;
+
+public interface PetTypeService extends CrudService<PetType, Long>{
+
+}
---
+++ pet-clinic-data/src/main/java/guru/springframework/vinpetclinic/services/map/PetTypeMapService.java
@@ -0,0 +1,38 @@
+package guru.springframework.vinpetclinic.services.map;
+
+import java.util.Set;
+
+import org.springframework.stereotype.Service;
+
+import guru.springframework.vinpetclinic.pojo.PetType;
+import guru.springframework.vinpetclinic.services.PetTypeService;
+
+@Service
+public class PetTypeMapService extends AbstractMapService<PetType, Long> implements PetTypeService {
+
+	@Override
+	public Set<PetType> findAll() {
+		return super.findAll();
+	}
+
+	@Override
+	public PetType findById(Long id) {
+		return super.findById(id);
+	}
+
+	@Override
+	public PetType save(PetType object) {
+		return super.save(object);
+	}
+
+	@Override
+	public void deleteById(Long id) {
+		super.deleteById(id);
+	}
+
+	@Override
+	public void delete(PetType object) {
+		super.delete(object);
+	}
+
+}

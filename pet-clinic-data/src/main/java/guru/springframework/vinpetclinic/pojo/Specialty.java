32dc7423e5c989f50923e397c3218bcf958c0cba
Vincenzo <vincenzolucente@gmail.com>
2020-03-26 08:50:59 +0100

    added Specialty POJO and linked to Vet

---
+++ pet-clinic-data/src/main/java/guru/springframework/vinpetclinic/pojo/Specialty.java
@@ -0,0 +1,14 @@
+package guru.springframework.vinpetclinic.pojo;
+
+public class Specialty extends BaseEntity {
+
+	private static final long serialVersionUID = 1L;
+	private String description;
+	
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+}
